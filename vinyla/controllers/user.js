da4a14931627b04a96038b9bddb4046b6f50e8c5
khl6235 <khl6235@naver.com>
2021-04-04 15:52:58 +0900

    util & user ctlr setting

---
+++ vinyla/controllers/user.js
@@ -0,0 +1,8 @@
+const util = require('../modules/util');
+const resMessage = require('../modules/responseMessage');
+const statusCode = require('../modules/statusCode');
+const jwt = require('../modules/jwt');
+
+module.exports = {
+    
+}
---
+++ vinyla/routes/user.js
@@ -0,0 +1,5 @@
+var express = require('express');
+var router = express.Router();
+const UserController = require('../../controllers/user');
+
+module.exports = router;

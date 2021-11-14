0869b58794fc3f3b388f8c325b5235f215952646
khl6235 <khl6235@naver.com>
2021-11-14 11:54:28 +0900

    FIX: authUtil file replace

--- vinyla/routes/vinyls/index.js
+++ vinyla/routes/vinyls/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 const VinylController = require('../../controllers/vinyl');
-const authUtil = require('../../modules/authUtil');
+const authUtil = require('../../middleware/authUtil');
 
 router.post('/', VinylController.save);
 router.get('/search', authUtil.checkToken, VinylController.search);
